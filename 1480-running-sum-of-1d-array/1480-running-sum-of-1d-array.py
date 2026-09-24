d3e1be4dbe5ee47d45ad3b1a28776021d4a1d968
shritikav <157160962+shritikav@users.noreply.github.com>
2026-09-24 17:23:19 +0530

    Delete 1480-running-sum-of-1d-array directory

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def runningSum(self, nums: list[int]) -> list[int]:
-        ans=[]
-        sum=0
-        for i in nums:
-            sum+=i
-            ans.append(sum)
-        return ans
